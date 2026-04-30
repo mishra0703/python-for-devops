94949043fcf7441e4553087f2a4f890d4e9fd14a
Prem Mishra <premmishra874478@gmail.com>
2026-04-30 14:27:22 +0530

    Day-04 Task Completed

---
+++ day-04/log_analyzer.py
@@ -0,0 +1,48 @@
+def read_logs():
+    with open("app.log","r") as log_file:
+        return log_file.readlines()
+
+
+def count_logs():
+    log_count = {"INFO" : 0 , "WARNING" : 0 , "ERROR" : 0}
+    logs = read_logs()
+
+    for line in logs:
+        if "INFO" in line:
+            log_count["INFO"]+=1
+        if "WARNING" in line:
+            log_count["WARNING"]+=1
+        if "ERROR" in line:
+            log_count["ERROR"]+=1
+
+    return log_count    
+
+
+def summary_file(logs):
+
+    report = []
+
+    report.append("\n----- Summary Report -----\n")
+
+    report.append("| State    | Count |\n")
+    report.append(f"  Info        {logs['INFO']}\n")
+    report.append(f"  Warning     {logs['WARNING']}\n")
+    report.append(f"  Errors      {logs['ERROR']}\n")   
+
+
+    if logs['ERROR'] > 15:
+        report.append("\n\n----- SUGGESTIONS -----\n")
+        report.append("!! Debug your code now !!\n")
+    elif logs['WARNING'] > 10:
+        report.append("\n\n----- SUGGESTIONS -----\n")
+        report.append("You need to review your code\n")
+
+    return "".join(report)
+
+
+Summary_Report = summary_file(count_logs())
+print(Summary_Report)       # Output in Terminal
+
+
+with open("log_summary.txt","w") as log_report:
+    log_report.write(Summary_Report)        # Output in .txt File
